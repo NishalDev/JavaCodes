5aae2478e56638a2c0415511ce89edacb7a82eac
Nishal <nishaldevadiga2003@gmail.com>
2024-10-07 12:39:15 +0530

    HashMap2

---
+++ HashMap/Main1.java
@@ -0,0 +1,26 @@
+package HashMap;
+
+import java.util.*;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int[] nums = {3,4,4,4,2 };
+        int res = majorityElement(nums);
+        System.out.println(res);
+    }
+
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        int res = 0;
+        int majority = 0;
+        for (int n : nums) {
+            hash.put(n, 1 + hash.getOrDefault(n, 0));
+            if (hash.get(n) > majority) {
+                res = n;
+                majority = hash.get(n);
+            }
+        }
+
+        return res;
+    }
+}
